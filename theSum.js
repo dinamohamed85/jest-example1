84399fead8da0753855645a2d1de776d934fbc92
dinamohamed85 <dina.elghareeb@gmail.com>
2022-10-29 23:58:47 +0200

    fix

--- theSum.js
+++ theSum.js
@@ -1,5 +1,5 @@
 function theSum(...numbers) {
-    return numbers.reduce((prvV, curV) => prvV + curV, 0);
+    return numbers.reduce((prv_Val, curr_Val) => prv_Val + curr_Val, 0);
 }
 
 module.exports = theSum
